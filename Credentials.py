d51b7ef5232cecd13d22f941ac15d92ce633d695
story105 <story105@mail.chapman.edu>
2019-04-10 10:59:31 -0700

    Create Credentials.py
    Here we can consolidate access keys

---
+++ Credentials.py
@@ -0,0 +1,5 @@
+#Credentials for SOUNDLINK (SongMatch)
+
+#Youtube API through google APIs
+ClientIDYouTube = "867034448406-p30qpbuaq1mc23lrncf1fd3e9vunb06l.apps.googleusercontent.com"
+ClientSecretYouTube = "Dvuz0aaWr29tTklfD8j2AIMX"
